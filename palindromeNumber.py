d9cfd7b3cd23ddb0323f71f0e11b3ae4ac66b93d
Bryan English <bryanenglish@protonmail.com>
2020-04-22 20:34:54 -0500

    a bonkers 1 liner that I found

--- palindromeNumber.py
+++ palindromeNumber.py
@@ -4,21 +4,8 @@ import collections
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
 
-        y = list(str(x))
-
-        if '-' in y:
-            return False
-        else:
-             z = len(y)-1
-
-             for x in range(0, int(len(y)/2)):
-                 if y[x] is y[z]:
-                     z -= 1
-                 else:
-                     return False
-
-        return True
 
 s = Solution()
 print(s.isPalindrome(121))
